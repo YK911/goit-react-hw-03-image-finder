eba2d8b8ec662d75bdd3b1145110b18a66d4e6bd
YK911 <jaroslavkositsa@gmail.com>
2020-02-20 17:08:47 +0200

    changed the display of images on the page

--- src/services/image-api.js
+++ src/services/image-api.js
@@ -5,7 +5,7 @@ import axios from "axios";
 export const fetchImages = async (query, currentPage = 1) => {
   try {
     return await axios.get(
-      `${process.env.REACT_APP_BASE_URL}?q=${query}&page=${currentPage}&key=${process.env.REACT_APP_API_KEY}&image_type=photo&orientation=horizontal&per_page=4`
+      `${process.env.REACT_APP_BASE_URL}?q=${query}&page=${currentPage}&key=${process.env.REACT_APP_API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
     );
   } catch (error) {
     console.error("FETCH ERROR: ", error);
